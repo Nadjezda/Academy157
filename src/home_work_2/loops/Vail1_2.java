929605a06124eccb6a436fb4130f8c2a1ee6c855
Nadzejda Liulkovich <farti-all@mail.ru>
2025-11-05 21:18:58 +0300

    изменение на аргумент и добавление точки

--- src/home_work_2/loops/Vail1_2.java
+++ src/home_work_2/loops/Vail1_2.java
@@ -9,13 +9,12 @@ public class Vail1_2 {
     //		1.2.2 Пример: Ввели 99.2, должно получиться в консоли: Введено не целое число
     //		1.2.3 Пример: Ввели Привет, должно получиться в консоли: Введено не число
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Введите целое число ");
-        String enterNumber = console.nextLine();// сохраняем число в строке
+
+        String enterNumber = args[0];// сохраняем число в строке
         String reversedString = new StringBuilder(enterNumber).reverse().toString(); // переворачиваем строку
         long composition = 1; //для сбора результата умножения
 
-        if (reversedString.contains(",")) {
+        if (reversedString.contains(",") || reversedString.contains(".")) {
             System.out.println("Введено не целое число");// проверяем содержит ли строка запятую
         } else if (reversedString.matches("[a-zA-Z]+") || reversedString.matches("[а-яА-Я]+")) { // проверяем слово ли
             System.out.println("Введено не число");
